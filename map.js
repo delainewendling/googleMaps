6596c8c320e6a4bccf58030b550073c8449d20f3
Delaine Wendling <delaine.wendling@gmail.com>
2016-08-17 10:59:31 -0500

    Added multiple markers and animation

--- map.js
+++ map.js
@@ -1,25 +1,8 @@
 // Create a map variable
 var map;
 
-// var styles = [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"poi","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"transit.line","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"labels.text.fill","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit.station","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"transit.station","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}]
-var styles = [
-  {
-    "featureType": "poi.attraction",
-    "stylers": [
-      { "visibility": "on" },
-      { "color": "#ff962a" },
-      { "hue": "#ffdd00" }
-    ]
-  },
-  {
-    "featureType": "landscape.natural.terrain",
-    "stylers": [
-      { "visibility": "on" },
-      { "color": "#ff962a" },
-      { "hue": "#ffdd00" }
-    ]
-  }
-]
+var styles = [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"poi","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"transit.line","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"labels.text.fill","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit.station","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"transit.station","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}]
+
 // Function to initialize the map within the map div
 function initMap() {
  map = new google.maps.Map(document.getElementById('map'), {
@@ -27,61 +10,117 @@ function initMap() {
    zoom: 10,
    styles: styles
 });
- // var trafficLayer = new google.maps.TrafficLayer();
- //  trafficLayer.setMap(map);
-   // setMarkers(map);
-  // var image = 'https://d30y9cdsu7xlg0.cloudfront.net/png/39868-200.png'
-  var empireStateMarker = new google.maps.Marker({
-        position: {lat: 40.7128, lng: -74.0059},
-        map: map,
-        title: "Empire State"
-  });
+
+ var monuments = [
+  {title:'Empire State', location: {lat: 40.7484, lng: -73.9857}},
+  {title: 'Statue of Liberty', location: {lat: 40.6892, lng: -74.0445}},
+  {title:'Grand Army Plaza', location:{lat: 40.7644, lng: -73.9734}},
+  {title: 'Columbus Circle Fountain', location: {lat: 40.7851, lng: -73.9683}},
+  {title: '911 Memorial', location: {lat: 40.7116, lng: -74.0132}}
+];
+
+var largeInfowindow = new google.maps.InfoWindow();
+var markers = []
+
+for (i=0; i < monuments.length; i++){
+  var position = monuments[i].location;
+  var title = monuments[i].title;
   var marker = new google.maps.Marker({
-    position: {lat: 40.7028, lng: -74.128},
-    map: map,
-    title: 'First Marker'
+    position: position,
+    title: title,
+    //anitmation for the markers
+    animation: google.maps.Animation.DROP,
+    id: i
   });
-
-  var empireContent = `<div id="content">
-      <div id="siteNotice">
-      </div> <h1 id="firstHeading" class="firstHeading">Empire State Building</h1>
-      <div id="bodyContent"><p>The tallest building in the United States located at ${marker.position}</p>`
-  var empireInfo = new google.maps.InfoWindow({
-    content: empireContent,
-    maxwidth: 50
-  });
- // TODO: create a single infowindow, with your own content.
- // It must appear on the marker
-  var contentString = `<div id="content">
-      <div id="siteNotice">
-      </div> <h1 id="secondHeading" class="firstHeading">New York</h1>
-      <div id="bodyContent"><p>The largest city in the United States located at ${marker.position}</p>`
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString,
-    maxwidth: 50
-  });
-
- // TODO: create an EVENT LISTENER so that the infowindow opens when
- // the marker is clicked!
- marker.addListener('click', function() {
-    infowindow.open(map, marker);
-    empireInfo.close()
-  });
- empireStateMarker.addListener('click', function() {
-    empireInfo.open(map, empireStateMarker);
-    infowindow.close()
+  //pushes each of the markers created into the markers array
+  markers.push(marker)
+  marker.addListener('click', function(){
+    toggleBounce(this)
+    populateInfoWindow(this, largeInfowindow);
   });
 }
+  function populateInfoWindow(marker, infowindow){
+    if(infowindow.marker != marker) {
+      infowindow.marker = marker;
+      infowindow.setContent('<div>' + marker.title + "</div>");
+      infowindow.open(map, marker);
+      infowindow.addListener("closeclick", function(){
+        infowindow.setMarker(null)
+      })
+    }
+  }
+
+  function toggleBounce(marker){
+    if(marker.getAnimation()!==null){
+      marker.setAnimation(null)
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE)
+    }
+  }
+
+  document.getElementById('show-monuments').addEventListener('click', showMonuments)
+  document.getElementById('hide-monuments').addEventListener('click', hideMonuments)
+
+  function showMonuments(){
+    var bounds = new google.maps.LatLngBounds()
+    for (i=0; i<markers.length; i++){
+      markers[i].setMap(map);
+     //extends the boundaries of the map for each marker
+    bounds.extend(markers[i].position)
+    }
+  map.fitBounds(bounds)
+  }
+  function hideMonuments(){
+    for (i=0; i<markers.length; i++){
+      markers[i].setMap(null);
+    }
+  }
+}
+ //  var empireStateMarker = new google.maps.Marker({
+ //        position: {lat: 40.7128, lng: -74.0059},
+ //        map: map,
+ //        title: "Empire State"
+ //  });
+ //  var marker = new google.maps.Marker({
+ //    position: {lat: 40.7028, lng: -74.128},
+ //    map: map,
+ //    title: 'First Marker'
+ //  });
+
+ //  var empireContent = `<div id="content">
+ //      <div id="siteNotice">
+ //      </div> <h1 id="firstHeading" class="firstHeading">Empire State Building</h1>
+ //      <div id="bodyContent"><p>The tallest building in the United States located at ${marker.position}</p>`
+ //  var empireInfo = new google.maps.InfoWindow({
+ //    content: empireContent,
+ //    maxwidth: 50
+ //  });
+ // // TODO: create a single infowindow, with your own content.
+ // // It must appear on the marker
+ //  var contentString = `<div id="content">
+ //      <div id="siteNotice">
+ //      </div> <h1 id="secondHeading" class="firstHeading">New York</h1>
+ //      <div id="bodyContent"><p>The largest city in the United States located at ${marker.position}</p>`
+ //  var infowindow = new google.maps.InfoWindow({
+ //    content: contentString,
+ //    maxwidth: 50
+ //  });
+
+ // // TODO: create an EVENT LISTENER so that the infowindow opens when
+ // // the marker is clicked!
+ // marker.addListener('click', function() {
+ //    infowindow.open(map, marker);
+ //    empireInfo.close()
+ //  });
+ // empireStateMarker.addListener('click', function() {
+ //    empireInfo.open(map, empireStateMarker);
+ //    infowindow.close()
+ //  });
+// }
 
 // Data for the markers consisting of a name, a LatLng and a zIndex for the
 // order in which these markers should display on top of each other.
-//  var monuments = [
-//   ['Empire State', 40.7484, -73.9857, 4],
-//   ['Statue of Liberty', 40.6892, -74.0445, 5],
-//   ['Grand Army Plaza', 40.7644, -73.9734, 3],
-//   ['Columbus Circle Fountain', 40.7851, -73.9683, 2],
-//   ['911 Memorial', 40.7116, -74.0132, 1]
-// ];
+//  
 
 // function setMarkers(map) {
 // // Adds markers to the map.
@@ -117,5 +156,8 @@ function initMap() {
 //     });
 //   }
 // }
- 
+ // var trafficLayer = new google.maps.TrafficLayer();
+ //  trafficLayer.setMap(map);
+   // setMarkers(map);
+  // var image = 'https://d30y9cdsu7xlg0.cloudfront.net/png/39868-200.png'
 // }
